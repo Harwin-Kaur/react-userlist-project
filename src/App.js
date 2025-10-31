61772e2e34d4546ab70d7639640a18629470517d
Harwin <sandhuharwinkaur@gmail.com>
2025-10-31 23:46:04 +1100

    added form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import  { UserForm }  from './components/UserForm';
 
 const App = () => {
    
     return ( <div>
       {/* form component here */}
-
+      <UserForm />
 
       {/* table component here */}
     </div>
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const UserForm = () => {
+export const UserForm = () => {
   return (
-    <div>UserForm</div>
+    <div className='w-50 shadow-lg p-5 m-auto mt-5 rounded'>
+      <form action="">
+        <div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name" />
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" />
+  </div>
+</div>
+      </form>
+    </div>
   )
 }
 
