10fd684a64e0dae2603265f2376129a70d80582e
Harwin <sandhuharwinkaur@gmail.com>
2025-11-01 20:39:01 +1100

    added UserTable component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import  { UserForm }  from './components/UserForm';
+import { UserTable } from './components/UserTable';
 
 const App = () => {
    
-    return ( <div>
+    return ( 
+    <div className='container pt-4'>
+      <h2 className='text-center'>User List</h2>
+      <hr />
       {/* form component here */}
       <UserForm />
 
       {/* table component here */}
+      <UserTable />
     </div>
     );
 }
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,15 +2,23 @@ import React from 'react'
 
 export const UserForm = () => {
   return (
-    <div className='w-50 shadow-lg p-5 m-auto mt-5 rounded'>
+    <div className='w-50 shadow-lg p-3 m-auto mt-5 rounded'>
       <form action="">
-        <div class="row">
-  <div class="col">
-    <input type="text" class="form-control" placeholder="First name" aria-label="First name" />
+        <div className="row g-2">
+  <div className="col-md-3">
+    <select className="form-select" id="inputState" name='gender' required defaultValue="">
+      <option selected>Choose...</option>
+      <option value="m">Male</option>
+      <option value="f">Female</option>
+    </select>
   </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" />
+  <div className="col-md-6">
+    <input name='name' type="text" className="form-control" placeholder="User name" aria-label="User name" required/>
   </div>
+      <div className='col-md-3 d-grid'>
+    <button className='button.btn.btn-primary'>Add User</button>
+
+      </div>
 </div>
       </form>
     </div>
