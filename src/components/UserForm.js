fcb14b32ee6e88e70c0424121302601697dd167f
Harwin <sandhuharwinkaur@gmail.com>
2025-11-02 23:19:28 +1100

    added data using state

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const UserForm = () => {
+
+  const [user, setUser] = useState({});
+
+  const handleOnChange = (e) => {
+    const {name, value} = e.target;
+    console.log(name, value);
+
+    setUser({
+      [name]: value,
+    })
+  };
+console.log(user);
+
   return (
     <div className='w-50 shadow-lg p-3 m-auto mt-5 rounded'>
       <form action="">
         <div className="row g-2">
   <div className="col-md-3">
-    <select className="form-select" id="inputState" name='gender' required defaultValue="">
-      <option selected>Choose...</option>
+    <select className="form-select" id="inputState" name='gender' required defaultValue="" onChange={handleOnChange}>
+      <option value="">Choose...</option>
       <option value="m">Male</option>
       <option value="f">Female</option>
     </select>
   </div>
   <div className="col-md-6">
-    <input name='name' type="text" className="form-control" placeholder="User name" aria-label="User name" required/>
+    <input name='name' type="text" className="form-control" placeholder="User name" aria-label="User name" onChange={handleOnChange} required/>
   </div>
       <div className='col-md-3 d-grid'>
     <button className='button.btn.btn-primary'>Add User</button>
